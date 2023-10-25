176140051b6a297ab7049dfe107b7cb3b48eb828
Luca Costanza <luca@zuru.tech>
2023-10-25 10:30:31 +0200

    fix xcode project

---
+++ SteamWebCPP/SteamWebCPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SteamWebCPP
+//
+//  Created by Luca Costanza Old on 25/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
